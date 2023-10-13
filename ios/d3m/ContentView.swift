40c0fb5ece1384ec2e7951db68b65a4989a465f5
Heechang Yang <yangtopia@bbros.kr>
2023-10-13 18:57:09 +0900

    chore: ios 프로젝트 추가

---
+++ ios/d3m/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  d3m
+//
+//  Created by yangtopia on 2023/10/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios/d3m/d3mApp.swift
@@ -0,0 +1,17 @@
+//
+//  d3mApp.swift
+//  d3m
+//
+//  Created by yangtopia on 2023/10/13.
+//
+
+import SwiftUI
+
+@main
+struct d3mApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
